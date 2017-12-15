1302a9e91dbfcb7cf9374f629ca9076ff6930804
Michelle <michelleannecampbell@gmail.com>
2017-12-15 09:51:24 -0500

    Merge branch 'master' of https://github.com/michelleannecampbell/ravenous

--- src/components/Business.js
+++ src/components/Business.js
@@ -1,41 +1,28 @@
 import React from 'react';
-import styles from './Business.css';
-
-let business = {
-  imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
-  name: 'MarginOtto Pizzeria',
-  address: '1010 Paddington Way',
-  city: 'Flavortown',
-  state: 'NY',
-  zipCode: '10101',
-  category: 'Italian',
-  rating: 4.5,
-  reviewCount: 90
-};
+import './Business.css';
 
 class Business extends React.Component {
   render() {
     return (
       <div className="Business">
         <div className="image-container">
-          <img src={business.thisImageSrc} alt='' />
+          <img src={this.props.business.imageSrc} alt=''/>
         </div>
-        <h2>{business.thisName}</h2>
+        <h2>{this.props.business.name}</h2>
         <div className="Business-information">
           <div className="Business-address">
-            <p>{business.thisAdress}</p>
-            <p>{business.thisCity}</p>
-            <p>{business.thisState, business.thisZipCode}</p>
+            <p>{this.props.business.address}</p>
+            <p>{this.props.business.city}</p>
+            <p>{`${this.props.business.state} ${this.props.business.zipCode}`}</p>
           </div>
           <div className="Business-reviews">
-            <h3>{business.thisCategory}</h3>
-            <h3 className="rating">{business.thisRating}</h3>
-            <p>business.thisReviewCount</p>
+            <h3>{this.props.business.category.toUpperCase()}</h3>
+            <h3 className="rating">{`${this.props.business.rating} stars`}</h3>
+            <p>{`${this.props.business.reviewCount} reviews`}</p>
           </div>
         </div>
       </div>
     );
-
   }
 }
 
--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -1,20 +1,20 @@
 import React from 'react';
-import styles from './BusinessList.css';
+import './BusinessList.css';
+
 import Business from '../Business';
 
-class BusinessList extends React.Component{
-  render(){
+class BusinessList extends React.Component {
+  render() {
     return (
-      <div className={styles.BusinessList}>
-        <Business />
-        <Business />
-        <Business />
-        <Business />
-        <Business />
-        <Business />
-      </div>
-    );
-  }
+      <div className="BusinessList">
+       {
+          this.props.businesses.map(business => {
+          return  <Business business={business} />
+          })
+       }
+       </div>
+      );
+}
 }
 
 export default BusinessList;
